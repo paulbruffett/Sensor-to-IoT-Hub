d1a9a8f8b4372915796c4015124579fa06fd6945
Paul Bruffett <paulbruffett@protonmail.com>
2021-03-07 12:22:15 -0800

    first commit

---
+++ IoT_Hub_Telemetry.ino
@@ -0,0 +1,176 @@
+#include "arduino_secrets.h"
+/*
+  Azure IoT Hub WiFi
+
+  This sketch securely connects to an Azure IoT Hub using MQTT over WiFi.
+  It uses a private key stored in the ATECC508A and a self signed public
+  certificate for SSL/TLS authetication.
+
+  It publishes a message every 5 seconds to "devices/{deviceId}/messages/events/" topic
+  and subscribes to messages on the "devices/{deviceId}/messages/devicebound/#"
+  topic.
+
+  The circuit:
+  - Arduino MKR WiFi 1010 or MKR1000
+
+  This example code is in the public domain.
+*/
+#include <ArduinoLowPower.h>
+#include <ArduinoBearSSL.h>
+#include <ArduinoECCX08.h>
+#include <utility/ECCX08SelfSignedCert.h>
+#include <ArduinoMqttClient.h>
+#include <WiFiNINA.h> // change to #include <WiFi101.h> for MKR1000
+#include <Arduino_MKRENV.h>
+#include <ArduinoJson.h>
+
+/////// Enter your sensitive data in arduino_secrets.h
+const char ssid[]        = SECRET_SSID;
+const char pass[]        = SECRET_PASS;
+const char broker[]      = SECRET_BROKER;
+String     deviceId      = SECRET_DEVICEID;
+int led_state = 0;
+
+int status = WL_IDLE_STATUS;
+
+WiFiClient    wifiClient;            // Used for the TCP socket connection
+BearSSLClient sslClient(wifiClient); // Used for SSL/TLS connection, integrates with ECC508
+MqttClient    mqttClient(sslClient);
+
+unsigned long lastEpoch = 0;
+
+void setup() {
+
+  if (!ENV.begin()) {
+    Serial.println("Failed to initialize MKR ENV shield!");
+    while (1);
+  }
+
+  if (!ECCX08.begin()) {
+    Serial.println("No ECCX08 present!");
+    while (1);
+  }
+
+  // reconstruct the self signed cert
+  ECCX08SelfSignedCert.beginReconstruction(0, 8);
+  ECCX08SelfSignedCert.setCommonName(ECCX08.serialNumber());
+  ECCX08SelfSignedCert.endReconstruction();
+
+  // Set a callback to get the current time
+  // used to validate the servers certificate
+  ArduinoBearSSL.onGetTime(getTime);
+
+  // Set the ECCX08 slot to use for the private key
+  // and the accompanying public certificate for it
+  sslClient.setEccSlot(0, ECCX08SelfSignedCert.bytes(), ECCX08SelfSignedCert.length());
+
+  // Set the client id used for MQTT as the device id
+  mqttClient.setId(deviceId);
+
+
+
+  mqttClient.setUsernamePassword(SECRET_USERNAME, SECRET_PASSWORD);
+
+  // Set the message callback, this function is
+  // called when the MQTTClient receives a message
+  mqttClient.onMessage(onMessageReceived);
+
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+    // WiFi.lowPowerMode();  // ENABLE WiFi Low Power Mode
+    delay(5000);
+  }
+}
+
+void loop() {
+
+  if (!mqttClient.connected()) {
+    // MQTT client is disconnected, connect
+    connectMQTT();
+  }
+
+  // poll for new MQTT messages and send keep alives
+  mqttClient.poll();
+
+  publishMessage();
+
+  if(led_state == 0){  
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  led_state =1;
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+    led_state = 0;
+  }
+  
+
+  // wait one minute
+  delay(60000);
+}
+
+unsigned long getTime() {
+  // get the current time from the WiFi module
+  return WiFi.getTime();
+}
+
+
+
+void connectMQTT() {
+  Serial.print("Attempting to MQTT broker: ");
+  Serial.print(broker);
+  Serial.println(" ");
+
+  while (!mqttClient.connect(broker, 8883)) {
+    // failed, retry
+    Serial.print(".");
+    Serial.println(mqttClient.connectError());
+    delay(5000);
+  }
+  Serial.println();
+
+  Serial.println("You're connected to the MQTT broker");
+  Serial.println();
+
+  // subscribe to a topic
+  mqttClient.subscribe("devices/" + deviceId + "/messages/devicebound/#");
+}
+
+void publishMessage() {
+  StaticJsonDocument<300> doc;
+  Serial.println("Publishing message");
+  float temperature = ENV.readTemperature(FAHRENHEIT);
+  float humidity    = ENV.readHumidity();
+  float pressure    = ENV.readPressure(PSI);
+  float illuminance = ENV.readIlluminance(FOOTCANDLE);
+  
+  // send message, the Print interface can be used to set the message contents
+  mqttClient.beginMessage("devices/" + deviceId + "/messages/events/");
+  doc["deviceId"] = deviceId;
+  doc["timestamp"] = getTime();
+  doc["temp"] = temperature;
+  doc["humidity"] = humidity;
+  doc["pressure"] = pressure;
+  doc["illuminance"] = illuminance;
+  serializeJson(doc,Serial);
+  serializeJson(doc, mqttClient);
+  mqttClient.endMessage();
+}
+
+void onMessageReceived(int messageSize) {
+  // we received a message, print out the topic and contents
+  Serial.print("Received a message with topic '");
+  Serial.print(mqttClient.messageTopic());
+  Serial.print("', length ");
+  Serial.print(messageSize);
+  Serial.println(" bytes:");
+
+  // use the Stream interface to print the contents
+  while (mqttClient.available()) {
+    Serial.print((char)mqttClient.read());
+  }
+  Serial.println();
+
+  Serial.println();
+}
